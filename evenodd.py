a9c3841f8f36832424de63f9a1ae582e6e46aa86
kunal galav <kunalgalav1995@gmail.com>
2023-07-21 18:05:22 +0530

    i want add this

---
+++ evenodd.py
@@ -0,0 +1,6 @@
+# write a Python function that takes an integer as input and returns "Even" if the number is even, and "Odd" if it is odd.
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+    print("the is Even number")
+else:
+    print("the is Odd number")
